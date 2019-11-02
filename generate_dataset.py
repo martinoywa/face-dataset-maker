536c4353a391edbee70c82473fd387570c1c8a7c
Martin Oywa <martinoywa2@gmail.com>
2019-11-02 12:43:05 +0300

    add main generator

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,30 +1,37 @@
 import cv2
 from random import randint
 
-face_cascade = cv2.CascadeClassifier('cascades\data\haarcascade_frontalface_alt2.xml')
-cap = cv2.VideoCapture(0)
 
-while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    face = face_cascade.detectMultiScale(gray)
+def func():
 
-    for (x, y, w, h) in face:
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        
-        image_name = 'images\martin\my_image'+str(randint(0, 1000000))+'.png'
-        cv2.imwrite(image_name, roi_color)
+    face_cascade = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_alt2.xml')
+    cap = cv2.VideoCapture(0)
 
-        color = (0, 255, 0)
-        stroke = 2
-        end_cord_x = x + w
-        end_cord_y = y + h
-        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+    while True:
+        ret, frame = cap.read() # intialize frame
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # necessary for detection
+        print(gray)
+        face = face_cascade.detectMultiScale(gray)
 
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+        for (x, y, w, h) in face:
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+            
+            image_name = 'images/martin/my_image'+str(randint(0, 1000000))+'.png'
+            cv2.imwrite(image_name, roi_color)
 
-cap.release()
-cv2.destroyAllWindows()
+            color = (0, 255, 0)
+            stroke = 2
+            end_cord_x = x + w
+            end_cord_y = y + h
+            cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    func()
